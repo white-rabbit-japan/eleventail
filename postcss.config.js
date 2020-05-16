fc034a991a48ee6c47c9c13f66501846005dfcff
Max Hodges <Max@whiterabbitjapan.com>
2020-05-17 02:22:32 +0900

    add css tabs

--- postcss.config.js
+++ postcss.config.js
@@ -9,10 +9,15 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 })
 
+
 module.exports = {
+  
   plugins: [
     require('tailwindcss'),
+    require('postcss-nested'),
     require('autoprefixer'),
     ...process.env.NODE_ENV === 'production' ? [purgecss,  require('cssnano')] : []
   ]
+
+  
 };
