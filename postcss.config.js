19d43a18d0ca4a3936cd32174eb701770ed1967e
Max Hodges <Max@whiterabbitjapan.com>
2020-05-18 01:53:08 +0900

    remove cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -17,7 +17,7 @@ module.exports = {
     require('postcss-nested'),
     require('autoprefixer'),
     // ...process.env.NODE_ENV === 'production' ? [purgecss,  require('cssnano')] : []
-    ...process.env.NODE_ENV === 'production' ? [require('cssnano')] : []
+    // ...process.env.NODE_ENV === 'production' ? [require('cssnano')] : []
   ]
 
   
